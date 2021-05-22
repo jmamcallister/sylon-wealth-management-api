5cb48deff14f9a72197a193ee4e0a989e9287a30
Alex McAllister <jmamcallister@gmail.com>
2021-05-22 14:01:09 +0100

    Springboot skeleton

---
+++ src/main/java/io/sylon/wealth/WealthManagementApiApplication.java
@@ -0,0 +1,13 @@
+package io.sylon.wealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthManagementApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WealthManagementApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/sylon/wealth/WealthManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sylon.wealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthManagementApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
